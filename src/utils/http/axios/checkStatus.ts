8537e4b9201f2fd270f020b757b406374842ee47
cKey <35512826+colinin@users.noreply.github.com>
2021-11-03 22:46:02 +0800

    feat(axios): error response status fine grained

--- src/utils/http/axios/checkStatus.ts
+++ src/utils/http/axios/checkStatus.ts
@@ -58,20 +58,50 @@ export function checkStatus(status: number, msg: string): void {
 }
 
 export function checkResponse(response: any): void {
-  if (response.data && response.data.error) {
-    if (response.status === 401) {
-      const userStore = useUserStoreWidthOut();
-      userStore.setToken(undefined);
-      userStore.setSessionTimeout(true);
-    } else if (response.data.error_description) {
-      error(response.data.error_description);
-    } else if (response.data.error.details) {
-      error(response.data.error.details);
-    } else if (response.data.error.message) {
-      error(response.data.error.message);
-    }
-  } else {
-    const { t } = useI18n();
-    checkStatus(response.status, t('sys.api.apiRequestFailed'));
+  let errorJson = response.data.error;
+
+  // 会话超时
+  if (response.status === 401) {
+    const userStore = useUserStoreWidthOut();
+    userStore.setToken(undefined);
+    userStore.setSessionTimeout(true);
+    return;
   }
+
+  // abp框架抛出异常信息
+  if (response.headers['_abperrorformat'] === 'true') {
+    if (errorJson === undefined && response.data.type === 'application/json') {
+      const reader = new FileReader();
+      reader.onload = function (e) {
+        errorJson = JSON.parse(e.target?.result as string);
+        console.log(errorJson);
+        error(errorJson.error.message);
+      };
+      reader.readAsText(response.data);
+    } else {
+      error(errorJson.message);
+    }
+    return;
+  }
+
+  // oauth错误信息
+  if (response.data.error_description) {
+    error(response.data.error_description);
+    return;
+  }
+
+  // 其他错误
+  if (response.data.error.details) {
+    error(response.data.error.details);
+    return;
+  }
+
+  if (response.data.error.message) {
+    error(response.data.error.message);
+    return;
+  }
+
+  // 都没捕获到则提示默认错误信息
+  const { t } = useI18n();
+  checkStatus(response.status, t('sys.api.apiRequestFailed'));
 }
