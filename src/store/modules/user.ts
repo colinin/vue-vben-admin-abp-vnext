71ab9446a8733a40ea43a01df18ef9a4fb842432
cKey <35512826+colinin@users.noreply.github.com>
2021-11-05 21:23:27 +0800

    fix: remove logout api

--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -154,13 +154,6 @@ export const useUserStore = defineStore({
      * @description: logout
      */
     async logout(goLogin = false) {
-      if (this.getToken) {
-        try {
-          await doLogout();
-        } catch {
-          console.log('注销Token失败');
-        }
-      }
       this.setToken(undefined);
       this.setSessionTimeout(false);
       this.setUserInfo(null);
